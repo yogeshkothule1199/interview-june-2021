b01ef1e89eb4b3a01157a4dc951199899b7c046b
yogeshkothule1199 <86230954+yogeshkothule1199@users.noreply.github.com>
2021-06-22 12:31:11 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,33 @@
-package exam;
 
-public class ConvertToBinary {
+import java.io.*;
 
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+class GFG {
+	
+	static void decToBinary(int n)
+	{
+		
+		int[] binaryNum = new int[32];
+
+		
+		int i = 0;
+		while (n > 0) {
+		
+			binaryNum[i] = n % 2;
+			n = n / 2;
+			i++;
+		}
+
+		
+		for (int j = i - 1; j >= 0; j--)
+			System.out.print(binaryNum[j]);
+	}
+
+
+	public static void main(String[] args)
+	{
+		int n = 17;
+		decToBinary(n);
+	}
 }
+
+
