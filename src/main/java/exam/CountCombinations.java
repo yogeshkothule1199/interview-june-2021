3cd1f455c324b45410cbb8d36422a7e00a5cdbad
yogeshkothule1199 <86230954+yogeshkothule1199@users.noreply.github.com>
2021-06-22 12:49:16 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,10 +1,73 @@
-package exam;
+/
+#include <bits/stdc++.h>
+using namespace std;
 
-public class CountCombinations {
+void findNumbers(vector<int>& ar, int sum,
+				vector<vector<int> >& res, vector<int>& r,
+				int i)
+{
+	
+	if (sum == 0) {
+		res.push_back(r);
+		return;
+	}
 
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
-  }
+	
+	while (i < ar.size() && sum - ar[i] >= 0) {
 
+		r.push_back(ar[i]); 
+
+		
+		findNumbers(ar, sum - ar[i], res, r, i);
+		i++;
+
+		
+		r.pop_back();
+	}
 }
+vector<vector<int> > combinationSum(vector<int>& ar,
+									int sum)
+{
+	
+	sort(ar.begin(), ar.end());
+
+	// remove duplicates
+	ar.erase(unique(ar.begin(), ar.end()), ar.end());
+
+	vector<int> r;
+	vector<vector<int> > res;
+	findNumbers(ar, sum, res, r, 0);
+
+	return res;
+}
+
+
+int main()
+{
+	vector<int> ar;
+	ar.push_back(2);
+	ar.push_back(4);
+	ar.push_back(6);
+	ar.push_back(8);
+	int n = ar.size();
+
+	int sum = 8; 
+  vector<vector<int> > res = combinationSum(ar, sum);
+
+	
+	if (res.size() == 0) {
+		cout << "Emptyn";
+		return 0;
+	}
+
+	
+	for (int i = 0; i < res.size(); i++) {
+		if (res[i].size() > 0) {
+			cout << " ( ";
+			for (int j = 0; j < res[i].size(); j++)
+				cout << res[i][j] << " ";
+			cout << ")";
+		}
+	}
+}
+
